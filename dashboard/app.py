44e0dfc1b17e08ef12f00be26e2d63ecab008b8a
Bastiaan van Gilst <b.p.vangilst@uu.nl>
2023-10-28 23:00:20 +0200

    Refactor

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,62 +1,72 @@
+#Make choropleth map with folium and with plotly
+
+#CBS Open Data
+#https://www.cbs.nl/nl-nl/onze-diensten/open-data/statline-als-open-data/cartografie
+
+#Plotly
+#https://plotly.com/python/mapbox-county-choropleth/
+
+
+#Libraries
 import pandas as pd
-import plotly.express as px
-from flask import Flask, render_template_string, render_template
-from sqlalchemy import create_engine, text, inspect, Table
 import geopandas as gpd
 import folium
 import cbsodata
+import plotly.express as px
 
-app = Flask(__name__)
+# Find out which columns are available
+metadata = pd.DataFrame(cbsodata.get_meta('83765NED', 'DataProperties'))
 
-@app.route('/')
-def index():
-    # Your original code starts here
+# Download birth rates and delete spaces from regional identifiers
+data = pd.DataFrame(cbsodata.get_data('85039NED', select = ['WijkenEnBuurten', 'Codering_3', 'GeboorteRelatief_25']))
+data['Codering_3'] = data['Codering_3'].str.strip()
 
-    # Find out which columns are available
-    metadata = pd.DataFrame(cbsodata.get_meta('83765NED', 'DataProperties'))
+# Retrieve data with municipal boundaries from PDOK
+geodata_url = 'https://cartomap.github.io/nl/wgs84/gemeente_2023.geojson'
+municipal_boundaries = gpd.read_file(geodata_url)
 
-    # Download birth rates and delete spaces from regional identifiers
-    data = pd.DataFrame(cbsodata.get_data('83765NED', select = ['WijkenEnBuurten', 'Codering_3', 'GeboorteRelatief_25']))
-    data['Codering_3'] = data['Codering_3'].str.strip()
+# Link data from Statistics Netherlands to geodata
+municipal_boundaries = pd.merge(municipal_boundaries, data,
+                               left_on = "statcode", 
+                               right_on = "Codering_3")
 
-    # Retrieve data with municipal boundaries from PDOK
-    geodata_url = 'https://cartomap.github.io/nl/wgs84/gemeente_2023.geojson'
-    municipal_boundaries = gpd.read_file(geodata_url)
+#CRS: EPSG 4326 (web mercator projection wgs84)
+municipal_boundaries.crs
+municipal_boundaries = municipal_boundaries.to_crs(epsg = 4326)
 
-    # Link data from Statistics Netherlands to geodata
-    municipal_boundaries = pd.merge(municipal_boundaries, data,
-                                   left_on = "statcode",
-                                   right_on = "Codering_3")
 
-    # CRS: EPSG 3857 (web mercator projection wgs84)
-    municipal_boundaries.crs
-    municipal_boundaries = municipal_boundaries.to_crs(epsg = 4326)
+#First column: Geoid, geometry column and  data columns
+gdf_choro = municipal_boundaries.copy()
+gdf_choro['geoid'] = gdf_choro.index.astype(str)
+gdf_choro = gdf_choro[['geoid', 'geometry', 'statnaam', 'GeboorteRelatief_25']]
+gdf_choro.head(3)
 
-    # First column: Geoid, geometry column and data columns
-    gdf_choro = municipal_boundaries.copy()
-    gdf_choro['geoid'] = gdf_choro.index.astype(str)
-    gdf_choro = gdf_choro[['geoid', 'geometry', 'statnaam', 'GeboorteRelatief_25']]
+#Center
+nld_lat = 52.2136
+nld_lon = 5.291266
+nld_coordinates = (nld_lat, nld_lon)
 
-    # Center
-    nld_lat = 52.2130
-    nld_lon = 5.2794
-    nld_coordinates = (nld_lat, nld_lon)
+#Folium base map
+map_nld = folium.Map(location=nld_coordinates, tiles='cartodbpositron', zoom_start=6, control_scale=True)
 
-    # Folium base map
-    map_nld = folium.Map(location=nld_coordinates, tiles='cartodbpositron', zoom_start=6, control_scale=True)
+#Folium choropleth
+map_nld = folium.Map(location=nld_coordinates, tiles='cartodbpositron', zoom_start=6, control_scale=True)
+folium.Choropleth(geo_data=gdf_choro,
+                  data=gdf_choro,
+                  columns=['geoid', 'GeboorteRelatief_25'],
+                  key_on='feature.id',
+                  fill_color='Blues',                  
+                  legend_name='Geboorterelatief'
+                 ).add_to(map_nld)
+map_nld
 
-    # Folium choropleth
-    folium.Choropleth(geo_data=gdf_choro,
-                      data=gdf_choro,
-                      columns=['geoid', 'GeboorteRelatief_25'],
-                      key_on='feature.id',
-                      fill_color='Blues',
-                      legend_name='Geboorterelatief').add_to(map_nld)
-
-    # Your original code ends here
-
-    map_nld.save("templates/map.html")
-    return render_template("map.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#Plotly choropleth
+fig = px.choropleth_mapbox(gdf_choro,
+                           geojson=gdf_choro.__geo_interface__,#['geometry']
+                           locations=gdf_choro.geoid,
+                           color='GeboorteRelatief_25',
+                           featureidkey='properties.geoid',
+                           center={'lat': 52.213, 'lon':5.2794},
+                           mapbox_style='carto-positron',
+                           zoom=6)
+fig.show()
